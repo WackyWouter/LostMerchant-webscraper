2063d0c6d0a31eb1aeb368caeb91d373c4050bbb
Wouter Bosch <wfcbosch@gmail.com>
2022-06-24 22:12:54 +0100

    make it run endlessly

--- webscraping.py
+++ webscraping.py
@@ -14,9 +14,24 @@ import config
 # Set up Telegram bot
 bot = AsyncTeleBot(config.botToken)
 
+
 @bot.message_handler(commands=['help', 'start'])
 async def send_welcome(message):
     await bot.reply_to(message, "Hi! This is a test!")
+
+
+async def wait_until(minute_mark):
+    while True:
+        now_utc = datetime.utcnow()
+        current_minutes = now_utc.minute
+
+        print('current-Minutes: ' + str(current_minutes))
+
+        diff_in_seconds = (minute_mark - current_minutes) * 60
+        await asyncio.sleep(diff_in_seconds / 2)
+        print('diff in seconds: ' + str(diff_in_seconds))
+        if diff_in_seconds <= 0.1:
+            return
 
 
 async def item_scraper():
@@ -59,77 +74,89 @@ async def item_scraper():
 
     # TODO find a way to pick the right one when a merchant has multiple locations suggested
     # Part of that is removing the earlier wrong suggestion if a new suggestion (correct one) comes in
-    # TODO make it so that it runs continuously
-    while 55 > current_minutes >= 30:
-
-        result_leg = ''
-        result_epic = ''
-
-        found_new_item = False
-
-        try:
-            # Wait a few seconds to give the rapport items a change to show up
-            wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "span.Epic")))
-
-            # Load the content into BeautifulSoup
-            html = BeautifulSoup(driver.page_source, features='html.parser')
-
-            # Find all elements in the content matching the attrs and loop over them
-            leg_items = html.findAll('span', attrs={'class': 'item Legendary'})
-            for legendaryItem in leg_items:
-
-                # Navigate to the zone column of the table row
-                item = legendaryItem.parent
-                card = item.previous_sibling.previous_sibling.previous_sibling
-                location = card.previous_sibling.previous_sibling.previous_sibling
-
-                # Save the location
-                if location.get_text() not in leg_loc:
-                    leg_loc.append(location.get_text())
-                    found_new_item = True
-
-            # create legendary message
-            if len(leg_items) > 0:
-                result_leg = (
-                        str(len(leg_items)) +
-                        ' Legendary rapport items have been found at the following locations: '
-                        + ', '.join(leg_loc) + '. ')
-
-            # Find all elements in the content matching the attrs and loop over them
-            epic_items = html.findAll('span', attrs={'class': 'item Epic'})
-            for epicItem in epic_items:
-
-                # Navigate to the zone column of the table row
-                item = epicItem.parent
-                card = item.previous_sibling.previous_sibling.previous_sibling
-                location = card.previous_sibling.previous_sibling.previous_sibling
-
-                # Save the location
-                if location.get_text() not in epic_loc:
-                    epic_loc.append(location.get_text())
-                    found_new_item = True
-
-            # create Epic message
-            result_epic = (str(len(epic_items)) + ' Epic rapport items have been found at the following locations: '
-                           + ', '.join(epic_loc) + '.')
-
-            if found_new_item:
-                await bot.send_message(config.chatIdList[0], result_leg + result_epic)
-        except TimeoutException:
-            await bot.send_message(config.chatIdList[0], 'No items have been found yet. :(')
-
-        # Sleep for two minutes because we don't need to check every second
-        await asyncio.sleep(120)
-
+    while True:
         # Update the time
         now_utc = datetime.utcnow()
         current_minutes = now_utc.minute
 
-    # Close the browser
-    driver.quit()
+        print('start of loop current minutes: ' + str(current_minutes))
+
+        while 55 > current_minutes >= 30:
+
+            result_leg = ''
+            result_epic = ''
+
+            found_new_item = False
+
+            try:
+                # Wait a few seconds to give the rapport items a change to show up
+                wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "span.Epic")))
+
+                # Load the content into BeautifulSoup
+                html = BeautifulSoup(driver.page_source, features='html.parser')
+
+                # Find all elements in the content matching the attrs and loop over them
+                leg_items = html.findAll('span', attrs={'class': 'item Legendary'})
+                for legendaryItem in leg_items:
+
+                    # Navigate to the zone column of the table row
+                    item = legendaryItem.parent
+                    card = item.previous_sibling.previous_sibling.previous_sibling
+                    location = card.previous_sibling.previous_sibling.previous_sibling
+
+                    # Save the location
+                    if location.get_text() not in leg_loc:
+                        leg_loc.append(location.get_text())
+                        found_new_item = True
+
+                # create legendary message
+                if len(leg_items) > 0:
+                    result_leg = (
+                            str(len(leg_items)) +
+                            ' Legendary rapport items have been found at the following locations: '
+                            + ', '.join(leg_loc) + '. ')
+
+                # Find all elements in the content matching the attrs and loop over them
+                epic_items = html.findAll('span', attrs={'class': 'item Epic'})
+                for epicItem in epic_items:
+
+                    # Navigate to the zone column of the table row
+                    item = epicItem.parent
+                    card = item.previous_sibling.previous_sibling.previous_sibling
+                    location = card.previous_sibling.previous_sibling.previous_sibling
+
+                    # Save the location
+                    if location.get_text() not in epic_loc:
+                        epic_loc.append(location.get_text())
+                        found_new_item = True
+
+                # create Epic message
+                result_epic = (str(len(epic_items)) + ' Epic rapport items have been found at the following locations: '
+                               + ', '.join(epic_loc) + '.')
+
+                if found_new_item:
+                    await bot.send_message(config.chatIdList[0], result_leg + result_epic)
+            except TimeoutException:
+                await bot.send_message(config.chatIdList[0], 'No items have been found yet. :(')
+
+            # Sleep for two minutes because we don't need to check every second
+            await asyncio.sleep(120)
+
+            # Update the time
+            now_utc = datetime.utcnow()
+            current_minutes = now_utc.minute
+
+        print('after loop current minutes: ' + str(current_minutes))
+
+        # Sleep for 6min to make sure we are past the hour mark to make calculations easier
+        await asyncio.sleep(360)
+
+        # Sleep until we are back at the half hour mark
+        await wait_until(30)
+
 
 async def main():
-    await asyncio.gather(bot.infinity_polling(), item_scraper())
+    await asyncio.gather(bot.infinity_polling(timeout=60, request_timeout=60), item_scraper())
 
 
 if __name__ == '__main__':
